b8040f159457fc1114e24617cbaedadc7c81dc4c
TheFatDemon <its@thefatdemon.me>
2015-10-17 12:40:28 -0700

    Updated Stuff

--- src/java/io/github/warrenhigh/mc/smite/SmitePlugin.java
+++ src/java/io/github/warrenhigh/mc/smite/SmitePlugin.java
@@ -1,7 +1,19 @@
 package io.github.warrenhigh.mc.smite;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Created by Mario Segura on 10/17/15.
  */
-public class SmitePlugin {
+public class SmitePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
 }
