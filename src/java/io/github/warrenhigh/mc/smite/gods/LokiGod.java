4e6a009deefd5d4a47536b6b4d6ce4978940d96d
TheFatDemon <its@thefatdemon.me>
2015-10-18 02:34:45 -0700

    Finished Loki

--- src/java/io/github/warrenhigh/mc/smite/gods/LokiGod.java
+++ src/java/io/github/warrenhigh/mc/smite/gods/LokiGod.java
@@ -3,6 +3,8 @@ package io.github.warrenhigh.mc.smite.gods;
 import io.github.warrenhigh.mc.smite.God;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 /**
  * Created by Mario Segura on 10/17/15.
@@ -10,17 +12,20 @@ import org.bukkit.entity.Player;
 public class LokiGod extends God {
     @Override
     public void abilityOne(Player victim) {
-
+        victim.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 5*20, 1));
+        victim.sendMessage(ChatColor.GOLD + "[RPG] You have been attacked by Loki");
     }
 
     @Override
     public void abilityTwo(Player victim) {
-
+        victim.setHealth(victim.getHealth() - 3.0);
+        victim.sendMessage(ChatColor.GOLD + "[RPG] You have been attacked by Loki");
     }
 
     @Override
     public void abilityThree(Player player) {
-
+        player.setHealth(player.getMaxHealth());
+        player.sendMessage(ChatColor.GOLD + "[RPG] You have been bleesed by the Gods");
     }
 
     @Override
